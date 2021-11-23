762f4ca0e32d34dfb0bd1ec51b93a36644ec163b
Audino723 <rioaudino723@gmail.com>
2021-11-23 10:07:52 +0800

    revisi cfg quotation mark

---
+++ input.py
@@ -0,0 +1,12 @@
+def do_something(x):
+    if x == 0:
+        return 0
+    elif x + 4 == 1:
+        if True:
+            return 3
+        else:
+            return 2
+    elif x == 32:
+        return 4
+    else:
+        return "Doodoo"
