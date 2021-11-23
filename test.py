58dc0e40f30c7165b1d08b077330e9313688aab0
ravaattar <rava.naufal2014@gmail.com>
2021-11-24 06:43:03 +0700

    handle self and get array idx

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-if(a == ["asdf,",[],adf,3,[[]]]):
-	print("asdf")
+a = [1,2,3]
+	
+for i in range(len(a)):
+	a[i] += 3
+	
+for numb in a:
+	print(numb)
+	
+print(a is list)
+print(1 is int)
+print(isinstance(a, list))
